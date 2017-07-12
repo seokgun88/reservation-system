40aff4c7edaff83a5b135ed394cafe98d3efdb2c
Yeongseok Lee <sukgun28@naver.com>
2017-07-12 11:01:39 +0900

    Add get in api controller

--- reservation-service/src/main/java/com/ys/reservation/controller/CategoryAPIController.java
+++ reservation-service/src/main/java/com/ys/reservation/controller/CategoryAPIController.java
@@ -1,11 +1,17 @@
 package com.ys.reservation.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ys.reservation.domain.Category;
@@ -16,6 +22,11 @@ import com.ys.reservation.service.CategoryService;
 public class CategoryAPIController {
 	@Autowired
 	private CategoryService categoryService;
+
+	@GetMapping
+	public List<Category> getAll() {
+		return categoryService.getAll();
+	}
 
 	@DeleteMapping("/{id:[\\d]+}") // 숫자 받는 정규식
 	public void delete(@PathVariable Integer id) {
