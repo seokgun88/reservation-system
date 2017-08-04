3edf66571eda6986d4af957207da6638053560eb
Yeongseok Lee <sukgun28@naver.com>
2017-08-04 10:40:37 +0900

    [BE] Use imageService instead of imageDao in controller

--- reservation-service/src/main/java/com/ys/reservation/controller/ImageController.java
+++ reservation-service/src/main/java/com/ys/reservation/controller/ImageController.java
@@ -1,12 +1,5 @@
 package com.ys.reservation.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -17,20 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.ys.reservation.dao.ImageDao;
-import com.ys.reservation.domain.Image;
+import com.ys.reservation.service.ImageService;
 
 @Controller
 @RequestMapping("/admin/images")
 @PropertySource("classpath:/application.properties")
 public class ImageController {
-	private ImageDao imageDao;
+	private ImageService imageService;
 	@Value("${file.basedir}")
 	private String baseDir;
 
 	@Autowired
-	public ImageController(ImageDao fileDao) {
-		this.imageDao = fileDao;
+	public ImageController(ImageService imageService) {
+		this.imageService = imageService;
 	}
 
 	@GetMapping
@@ -39,45 +31,9 @@ public class ImageController {
 	}
 	
 	@PostMapping
-	public String create(
-			@RequestParam String relation,
-			@RequestParam int id,
-			@RequestParam MultipartFile file) {
-		if(file != null) {
-			String formattedDate = baseDir + File.separator + new SimpleDateFormat("yyyy" + File.separator + "MM" + File.separator + "dd").format(new Date());
-			File f = new File(formattedDate);
-			if(!f.exists()) {
-				f.mkdirs();
-			}
-			
-			String contentType = file.getContentType();
-			String originalFilename = file.getOriginalFilename();
-			long size = file.getSize();
-			
-			String uuid = UUID.randomUUID().toString();
-			String saveFileName = formattedDate + File.separator + uuid;
-            
-            try(
-            		InputStream in = file.getInputStream();
-            		FileOutputStream fos = new FileOutputStream(saveFileName);){
-            	int readCnt = 0;
-            	byte[] buffer = new byte[1024];
-            	while((readCnt = in.read(buffer)) != -1){
-            		fos.write(buffer, 0, readCnt);
-            	}
-            } catch(Exception e){
-            	e.printStackTrace();
-            }
-            Image image = new Image();
-            image.setFileName(originalFilename);
-            image.setUserId(id);
-            image.setSaveFileName(saveFileName);
-            image.setFileLength(size);
-            image.setContentType(contentType);
-            image.setDeleteFlag(0);
-            imageDao.insert(image);
-		}
-		
+	public String create(@RequestParam int id, @RequestParam MultipartFile file) {
+		MultipartFile[] files = { file };
+		imageService.create(id, files);
 		return "redirect:/admin/images";
 	}
 }
