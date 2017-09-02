0ca8bf6270259b28a2a3f1f18c535c91e257f3af
LeeJunyeol <chljy33@naver.com>
2017-09-02 18:10:30 +0900

    [BE] Implement modifyReservationType method in ReservationApiController

--- src/main/java/com/yg/reservation/controller/ReservationApiController.java
+++ src/main/java/com/yg/reservation/controller/ReservationApiController.java
@@ -2,9 +2,12 @@ package com.yg.reservation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yg.reservation.domain.Reservation;
@@ -29,7 +32,17 @@ public class ReservationApiController {
 			@AuthUser User user) {
 		Reservation reservation = reservationVo.getReservation();
 		reservation.setUser(user);
-		return reservationService.add(reservation, reservationVo.getProductId());
+		return reservationService.add(reservation,
+				reservationVo.getProductId());
+	}
+
+	@PutMapping("/{id:[\\d]+}")
+	public boolean modifyReservationType(@PathVariable int id,
+			@RequestBody Reservation reservation) {
+		if (reservation.getReservationType() == null || reservation.getReservationType() < 0) {
+			return false;
+		}
+		return reservationService.modifyReservationType(id, reservation.getReservationType());
 	}
 
 	@GetMapping("/my")
